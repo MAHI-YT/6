a4ce1ed2cf3b0edfd21a6ac34f29c782efbc10a4
MAHI-YT <scarystories576@gmail.com>
2026-01-29 15:13:58 +0500

    Update main-repo.js

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -33,7 +33,7 @@ async (conn, mek, m, { from, reply }) => {
 
         // Send image with caption
         await conn.sendMessage(from, {
-            image: { url: `https://files.catbox.moe/r1y5yk.jpg` },
+            image: { url: `https://www.imghippo.com/i/ks5451ZE.jpg` },
             caption: formattedInfo,
             contextInfo: { 
                 mentionedJid: [m.sender],
